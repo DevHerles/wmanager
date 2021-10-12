b0ecfd128b66e76c5802d9b77a2573efb5a1325d
HerlesINC <herles.incalla@gmail.com>
2021-10-12 10:10:47 -0500

    Add external monitor feature

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ No awesome fonts?
 ```bash
 sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf
 ```
+
+Launch to external monitor
+
+eDP-1 = Local monitor
+HDMI-1 = External monitor
+
+```bash
+xrandr --output eDP-1 --primary --mode 1600x900 --rotate normal --output HDMI-1 --mode 1920x1080 --rotate normal --right-of eDP-1
+```
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,6 +2,10 @@
 
 # Terminate already running bar instances
 killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
@@ -11,4 +15,7 @@ echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar_example.log
 # polybar bar2 >>/tmp/polybar2.log 2>&1 & disown
 polybar example >>/tmp/polybar_example.log 2>&1 & disown
 
+if [[ $(xrandr -q | grep 'HDMI-1 connected') ]]; then
+  polybar top_external >>/tmp/polybar_top_external.log 2>&1 & disown
+fi
 echo "Bars launched..."
